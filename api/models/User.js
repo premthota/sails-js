6ddbfe4de27452c25ea5427426de3823d3c3bb11
Prem Sai Thota <premurs77@gmail.com>
2019-04-29 12:04:34 +0530

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -23,29 +23,36 @@ module.exports = {
     },
 
 
+    
+    contact: { type: 'string',
+    required:true ,
+    isNumber:true,
+    min: 10 ,
+    max: 10,
+    },
+
+    email: { type: 'email',
+    required:true ,
+    unique:  true,
+    isEmail: true,
+    },
+
+    password: { type: 'string',
+    required:  true ,
+    minLength: 6 ,
+    maxLength: 49,
+    regex:    /^[a-zA-Z]$/i,
+    regex:    /^[0-9]$/i,
+    },
+    //customToJSON: function () {
+    //return _.omit(this, ['password'])
+    //},
     //lastname: { type: 'string',
     //required:true ,
     //minLength: 1 
     //},
+    },
 
-    //contact: { type: 'number',
-    //required:true ,
-    //minLength: 10 
-    //},
-
-    //email: { type: 'string',
-    //required:true ,
-    //unique: true,
-    //isEmail: true,
-    //},
-
-    //password: { type: 'string',
-    //required:true ,
-    //return _.isString(value) && value.length >= 6 && value.match(/[a-z]/i) && value.match(/[0-9]/);
-    //},
-    //customToJSON: function () {
-    //return _.omit(this, ['password'])
-    //},
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
     //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
@@ -55,24 +62,36 @@ module.exports = {
     //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
 
-  },
-  validationMessages: {
+    // To use sails hook validation in your API install the following module
+    //npm install --save sails-hook-validation
+  
+   validationMessages:{
     firstname: {
-      required: "Please type your firstname",
+      required: "Please type your Firstname",
     },
     lastname: {
-      required: "Please type your lastname",
+      required: "Please type your Lastname",
     },
     contact: {
-      required: "Please type your mobile number",
+      required: "Please type your Mobile number",
+      isNumber: "Enter your mobile number",
+      min: "Mobile Number should be ten digits",
+      max: "Mobile Number should be ten digits",
     },
     email: {
       required: "Please type your email",
+      isEmail:  "Enter the valid E-mail",
+      unique:   "This email is used , enter another one",
     },
     password: {
-      required: "Enter your password which should be 6 minimum length containing 
-       Atleast a number and an alaphabet!",
+      required: "Enter your password which should be 6 minimum length ,containing Atleast a number and an alaphabet!",
+      minLength:"Password should be atleast 6 characters",
+      maxLength:"Password should not be morethan 49 characters",
+      regex:    "Password should contain minimum one character and one number "
     }
-  },
-};
 
+   },
+
+  
+
+};
