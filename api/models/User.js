b680eed83dc8e42d0af591ed439fce3d34421c9a
Prem Sai Thota <premurs77@gmail.com>
2019-05-01 21:13:00 +0530

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -4,7 +4,6 @@
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
-
 module.exports = {
 
   tableName:"user",
@@ -18,40 +17,31 @@ module.exports = {
     required:true 
     },
 
-    lastname: { type: 'string',
-    required:true 
+    lastname: { type: 'string', 
     },
 
-  
-    contact: { type: 'string',
+    contact: { type: 'number',
     required:true ,
-    isNumber:true,
-    min: 10 ,
-    max: 10,
+    min:     6000000000 ,
+    max:     9999999999,
     },
 
-    email: { type: 'email',
+    email: { type: 'string',
     required:true ,
     unique:  true,
-    isEmail: true,
     },
 
     password: { type: 'string',
     required:  true ,
     minLength: 6 ,
     maxLength: 49,
-    regex:    /^[a-zA-Z]$/i,
-    regex:    /^[0-9]$/i,
-    },
-    //customToJSON: function () {
-    //return _.omit(this, ['password'])
-    //},
-    //lastname: { type: 'string',
-    //required:true ,
-    //minLength: 1 
-    //},
-    },
+    }
 
+    },
+       customToJSON: function () {
+    return _.omit(this, ['password'])
+    },
+  
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
     //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
@@ -60,9 +50,6 @@ module.exports = {
     //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
     //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
-    // To use sails hook validation in your API install the following module
-    //npm install --save sails-hook-validation
   
    validationMessages:{
     firstname: {
@@ -73,20 +60,17 @@ module.exports = {
     },
     contact: {
       required: "Please type your Mobile number",
-      isNumber: "Enter your mobile number",
       min: "Mobile Number should be ten digits",
       max: "Mobile Number should be ten digits",
     },
     email: {
       required: "Please type your email",
-      isEmail:  "Enter the valid E-mail",
       unique:   "This email is used , enter another one",
     },
     password: {
       required: "Enter your password which should be 6 minimum length ,containing Atleast a number and an alaphabet!",
       minLength:"Password should be atleast 6 characters",
       maxLength:"Password should not be morethan 49 characters",
-      regex:    "Password should contain minimum one character and one number "
     }
 
    },
@@ -94,3 +78,4 @@ module.exports = {
   
 
 };
+
